82fe29aa8872d9b3a665842938c5ab9bf259cecf
andre-gcosta <andre.gcosta1@gmail.com>
2025-08-14 10:51:14 -0300

    structure and layout added

--- src/app/components/deafult-login-layout/deafult-login-layout.component.ts
+++ src/app/components/deafult-login-layout/deafult-login-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-deafult-login-layout',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './deafult-login-layout.component.scss'
 })
 export class DeafultLoginLayoutComponent {
-
+  @Input() title: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secondaryBtnText: string = "";
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { DeafultLoginLayoutComponent } from '../../components/deafult-login-layout/deafult-login-layout.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [DeafultLoginLayoutComponent],
+  imports: [
+    DeafultLoginLayoutComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor(){
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
 }
